3600f25d1fdf635e0dccde1e60890197198e84fa
Rayson Kong <kong.rayson@gmail.com>
2021-03-17 17:03:00 +0800

    add addBlock Method and generate a blockchain

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+// A dummy hash function
 const lightningHash = (data) => {
     return data + "*";
 }
@@ -17,8 +17,23 @@ class Block {
 class Blockchain{
     constructor() {
         const genesis = new Block('gen-data', 'gen-hash', 'gen-lastHash');
-
         this.chain = [genesis];       
     }
 
+    addBlock(data) {
+        const lastHash = this.chain[this.chain.length-1].hash;
+
+        const hash = lightningHash(data + lastHash);
+
+        const block = new Block(data, hash, lastHash);
+
+        this.chain.push(block);
+    }
 }
+
+const simpleBlockchain = new Blockchain();
+simpleBlockchain.addBlock('one');
+simpleBlockchain.addBlock('two');
+simpleBlockchain.addBlock('three');
+
+console.log(simpleBlockchain)
